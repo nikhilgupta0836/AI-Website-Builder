afd1a4d6369af6d77d0568838a1708ad4573c575
Nikhil Gupta <157088211+nikhilgupta0836@users.noreply.github.com>
2026-03-23 02:14:25 +0530

    Update openRouter.js

--- server/config/openRouter.js
+++ server/config/openRouter.js
@@ -1,5 +1,5 @@
 const openRouterUrl = "https://openrouter.ai/api/v1/chat/completions"
-const model = "deepseek/deepseek-chat"
+const model = "deepseek/deepseek-chat:free"
 
 export const generateResponse = async (prompt) => {
     const res = await fetch(openRouterUrl, {
